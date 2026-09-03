fc01d96bea2fc90fdac3d5d5047dff8d966ab359
RitamMani <2k23.it2311399@gmail.com>
2026-09-03 16:08:14 +0530

    Try Android VR YouTube client

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -10,6 +10,7 @@ def download_youtube_audio(url :str) ->str:
     ydl_opts = {
         "format": "bestaudio/best",
         "js_runtimes": {"deno": {}},
+        "extractor_args": {"youtube": {"player_client": ["android_vr"]}},
         
         "outtmpl": output_path,
         "postprocessors": [
