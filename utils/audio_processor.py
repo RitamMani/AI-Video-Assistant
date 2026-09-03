8ed0093a53472d550d1bc6c4151f644eddc10ebf
RitamMani <2k23.it2311399@gmail.com>
2026-09-03 15:52:03 +0530

    Configure Deno runtime for YouTube

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -9,6 +9,7 @@ def download_youtube_audio(url :str) ->str:
     output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
     ydl_opts = {
         "format": "bestaudio/best",
+        "js_runtimes": {"deno": {}},
         "outtmpl": output_path,
         "postprocessors": [
             {
